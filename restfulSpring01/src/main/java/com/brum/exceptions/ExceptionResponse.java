81a7ec9c18ced5c8dc55049b4fba3d4b1c353823
rafael36832 <caianarafaelcabral@gmail.com>
2024-09-14 13:52:44 -0300

    Handler Exceptions

---
+++ restfulSpring01/src/main/java/com/brum/exceptions/ExceptionResponse.java
@@ -0,0 +1,25 @@
+package com.brum.exceptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ExceptionResponse implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Date timestamp;
+	
+	private String message;
+	
+	private String details;
+	
+	public ExceptionResponse(Date timestamp, String message, String details) {
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}	
+
+}
