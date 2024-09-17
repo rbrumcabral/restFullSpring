3de65d491815fdf0a03d6327708517111440c9de
rafael36832 <caianarafaelcabral@gmail.com>
2024-09-17 11:56:05 -0300

    Add anotation for future swagger

--- restfulSpring01/src/main/java/com/brum/controllers/UserController.java
+++ restfulSpring01/src/main/java/com/brum/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,31 +31,31 @@ public class UserController {
 
 	private Logger logger = LoggerFactory.getLogger(UserController.class);
 
-	@GetMapping("/{id}")
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> findById(@PathVariable("id") Long id) {
 		User response = this.service.findById(id);
 		return new ResponseEntity<User>(response, HttpStatus.OK);
 	}
 
-	@GetMapping
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<User>> findAll() {
 		List<User> response = this.service.findAll();
 		return new ResponseEntity<List<User>>(response, HttpStatus.OK);
 	}
 
-	@PostMapping
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> create(@Valid @RequestBody User user) {
 		User response = this.service.create(user);
 		return new ResponseEntity<User>(response, HttpStatus.CREATED);
 	}
 
-	@PutMapping
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> update(@Valid @RequestBody User user) {
 		User response = this.service.update(user);
 		return new ResponseEntity<User>(response, HttpStatus.OK);
 	}
 
-	@DeleteMapping("/{id}")
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<User> delete(@PathVariable("id") Long id) {
 		this.service.delete(id);
 		return ResponseEntity.noContent().build();
