2c95843862b2707c6028cb6ebbe84187b6e281af
rafael36832 <caianarafaelcabral@gmail.com>
2024-09-25 14:49:39 -0300

    Content Negotiation yaml Header Param

--- restfulSpring01/src/main/java/com/brum/configs/WebConfig.java
+++ restfulSpring01/src/main/java/com/brum/configs/WebConfig.java
@@ -1,15 +1,22 @@
 package com.brum.configs;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import com.brum.serialization.YamlJackson2HttpMessageConverter;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	private static final MediaType MEDIA_TYPE_APPLICATION_YAML = MediaType.valueOf("application/x-yaml");
 
 	@Bean
 	LocaleChangeInterceptor localeChangeInterceptor() {
@@ -19,17 +26,19 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 
 	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(new YamlJackson2HttpMessageConverter());
+	}
+
+	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
-	
+
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		configurer.favorParameter(false)
-		.ignoreAcceptHeader(false)
-		.useRegisteredExtensionsOnly(false)
-		.defaultContentType(MediaType.APPLICATION_JSON)
-		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);
+		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML).mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YAML);
 	}
 }
--- restfulSpring01/src/main/java/com/brum/controllers/UserController.java
+++ restfulSpring01/src/main/java/com/brum/controllers/UserController.java
@@ -2,11 +2,8 @@ package com.brum.controllers;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.brum.domain.dto.v1.UserDTO;
 import com.brum.services.UserService;
+import com.brum.util.MediaType;
 
 import jakarta.validation.Valid;
 
@@ -29,27 +27,30 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
-	private Logger logger = LoggerFactory.getLogger(UserController.class);
-
-	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YAML })
 	public ResponseEntity<UserDTO> findById(@PathVariable("id") Long id) {
 		UserDTO response = this.service.findById(id);
 		return new ResponseEntity<UserDTO>(response, HttpStatus.OK);
 	}
 
-	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YAML })
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<UserDTO> response = this.service.findAll();
 		return new ResponseEntity<List<UserDTO>>(response, HttpStatus.OK);
 	}
 
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YAML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YAML })
 	public ResponseEntity<UserDTO> create(@Valid @RequestBody UserDTO user) {
 		UserDTO response = this.service.create(user);
 		return new ResponseEntity<UserDTO>(response, HttpStatus.CREATED);
 	}
 
-	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PutMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+			MediaType.APPLICATION_YAML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
+					MediaType.APPLICATION_YAML })
 	public ResponseEntity<UserDTO> update(@Valid @RequestBody UserDTO user) {
 		UserDTO response = this.service.update(user);
 		return new ResponseEntity<UserDTO>(response, HttpStatus.OK);
