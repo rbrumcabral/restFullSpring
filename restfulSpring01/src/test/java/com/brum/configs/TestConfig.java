0b7f67f78febb0fca1cfb629a2c8cfe2e503644f
rafael36832 <caianarafaelcabral@gmail.com>
2024-10-07 16:21:15 -0300

    Initial config TestContainers

---
+++ restfulSpring01/src/test/java/com/brum/configs/TestConfig.java
@@ -0,0 +1,14 @@
+package com.brum.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+	
+	public static final int SERVER_PORT = 8888;
+	
+	
+	
+	
+
+}
---
+++ restfulSpring01/src/test/java/com/brum/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.brum.integrationtests.testcontainers;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+
+	public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:9.0.1");
+
+		private static void startContainers() {
+			Startables.deepStart(Stream.of(mysql)).join();
+		}
+
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		@Override
+		public void initialize(ConfigurableApplicationContext applicationContext) {
+			startContainers();
+			ConfigurableEnvironment environment = applicationContext.getEnvironment();
+			MapPropertySource testcontainers = new MapPropertySource("testcontainers",
+					(Map) createConnectionConfiguration());
+			environment.getPropertySources().addFirst(testcontainers);
+		}
+
+		private static Map<String, String> createConnectionConfiguration() {
+			return Map.of(
+					"spring.datasource.url", mysql.getJdbcUrl(),
+					"spring.datasource.username", mysql.getUsername(),
+					"spring.datasource.password", mysql.getPassword()
+					);
+		}
+
+	}
+}
